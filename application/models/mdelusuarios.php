871c13bef5c70598a8f4c172339b0dba0b9d8403
srvinaesck <alejandro_servin@ucol.mx>
2012-05-16 17:13:49 -0500

    Update application/models/mdelusuarios.php

--- application/models/mdelusuarios.php
+++ application/models/mdelusuarios.php
@@ -3,6 +3,8 @@
         function dbborrar(){
         $datos = $this->input->post('usuario');
         $this->db->query("DELETE FROM tb_usuarios WHERE usuario='$datos'");
+        //modificación de esta mierda
+        
         }
     }
 ?>
